74c490c658be4032e0b9061a3f0dfe4a36e2a41b
sekharjpapp <43300292+sekharjpapp@users.noreply.github.com>
2024-08-04 20:05:15 +0530

    updated commit

--- src/main/java/com/pixel/controller/RoomController.java
+++ src/main/java/com/pixel/controller/RoomController.java
@@ -5,10 +5,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
